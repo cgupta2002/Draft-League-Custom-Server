b3d69498a57e62f0eb1cbf3e49c2912ae927292b
aeroflames <a3r0flames@gmail.com>
2025-12-15 10:19:55 -0500

    Trying to debug item issues.

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -141,8 +141,6 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 				item = this.dex.forGen(gen).items.get(item.id);
 				gen--;
 			}
-			console.log(this.ruleTable);
-			console.log(item);
 			if (requireObtainable && item.isNonstandard) {
 				if (this.ruleTable.has(`+item:${item.id}`)) return;
 				return [`${set.name}'s item ${item.name} does not exist in Gen ${this.dex.gen}.`];
